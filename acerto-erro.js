9bb37727d3426ab19131cebd59587a8b3d1f5f4a
rubenmartins10 <rubendavidsilvamartins@gmail.com>
2026-02-16 22:43:01 +0000

    Gestão do Feedback para o Utilizador

--- acerto-erro.js
+++ acerto-erro.js
@@ -6,7 +6,16 @@ function acerto(osCartoes) {
 
 function erro(osCartoes) {
     osCartoes.forEach(function(elemento) {
-        elemento.classList.remove("descoberta"); //remove a classe "descoberta" dos cartões que não foram acertados, para que o jogador possa tentar novamente
+        elemento.classList.add("erro"); //adiciona a classe "erro" aos cartões que não foram acertados, para indicar que o jogador errou
     });  
+
+    setTimeout(
+        function() {
+            osCartoes.forEach(function(elemento) {
+        elemento.classList.remove("descoberta"); //remove a classe "descoberta" dos cartões que não foram acertados, para que o jogador possa tentar novamente
+        elemento.classList.remove("erro"); //remove a classe "erro" dos cartões que não foram acertados, para que o jogador possa tentar novamente
+        
+        }); 
+    }, 1000); //sao 1000 milissegundos, ou seja, 1 segundo, para que o jogador tenha tempo de ver os cartões que ele errou antes de eles serem virados novamente para baixo    
 }
 
